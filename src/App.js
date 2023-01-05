59a663314fa12c93050adb65f8c815bf86091c37
Gabriel Camboim <gabrielcamboim99@gmail.com>
2023-01-05 11:56:02 -0300

    Alterações na pasta android detectadas

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Essdsdffdfddit <code>src/App.js</code> and save to reload.
+          Essdsdfdcffdfddit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
