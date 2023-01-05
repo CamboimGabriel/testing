58e0b560acab777c07e284d54ab807ad0978f77b
Gabriel Camboim <gabrielcamboim99@gmail.com>
2023-01-05 11:07:41 -0300

    testing commits

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Exdit <code>src/App.js</code> and save to reload.
+          Exxdit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
