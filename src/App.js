d6719a8dc057829a894026c75d32e836410fec9d
Gabriel Camboim <gabrielcamboim99@gmail.com>
2023-01-06 13:13:22 -0300

    NATIVE: testing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          edwewerit <code>src/App.js</code> and save to reload.
+          edit master <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
